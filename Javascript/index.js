b56d5b870794efeab1baf29be30216a44898706a
Peter <peter.gallwas@outlook.com>
2021-08-29 07:18:55 +1200

    1

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,5 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+/*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -9,4 +9,4 @@ function scrollFunction() {
         document.getElementById("navbar").style.padding = "80px 10px";
         document.getElementById("logo").style.fontSize = "35px";
     }
-}
+} */
